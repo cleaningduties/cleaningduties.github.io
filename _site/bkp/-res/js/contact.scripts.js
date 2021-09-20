44b3324db82c193a18ed72cf630854fa1f07dc9c
Alex Okros <alexandru.okros@live.com>
2021-09-20 16:25:17 +0300

    contact form

--- _site/bkp/-res/js/contact.scripts.js
+++ _site/bkp/-res/js/contact.scripts.js
@@ -1,13 +1,12 @@
-( function( $ ) {
+(function ($) {
+  "use strict";
 
-	'use strict';
+  var gFormSuccessTitle = "<h4>Va multumim pentru interesul acordat!</h4>";
+  var gFormSuccessMessage =
+    "<h6>Va vom contacta in cel mai scurt timp posibil.</h6>";
 
-	var gFormSuccessTitle = '<h4>Va multumim pentru interesul acordat!</h4>';
-	var gFormSuccessMessage = '<h6>Va vom contacta in cel mai scurt timp posibil.</h6>';
-
-	$('#gform').on('submit', function(e) {
-		$('#gform *').fadeOut(2000);
-	  $('#gform').prepend(gFormSuccessTitle+gFormSuccessMessage);
+  $("#contactForm").on("submit", function (e) {
+    $("#contactForm *").fadeOut(2000);
+    $("#contactForm").prepend(gFormSuccessTitle + gFormSuccessMessage);
   });
-
-} )(jQuery);
+})(jQuery);
